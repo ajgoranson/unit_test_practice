35770077f0f2132c4b7b7a33259441312e03fe1e
ajgoranson <sb9561bm@go.minnstate.edu>
2021-02-03 11:09:01 -0600

    Added an empty string value error and a string with numbers valueerror

---
+++ camel_case.py
@@ -0,0 +1,28 @@
+def camel_case(sentence):
+    """ Convert sentence to camelCase, for example, "Display all books"
+     is converted to "displayAllBooks" """
+    if sentence == '':
+        raise ValueError('Please enter a valid sentance')
+    if sentence.isdigit():
+        raise ValueError('No numbers please')
+    
+    title_case = sentence.title() # Uppercase first letter of each word
+    upper_camel_cased = title_case.replace(' ', '')# remove spaces 
+    # Lowercase first letter, join with rest of string
+    # Slices don't produce index out of bounds errors test
+    # So this still works on empty strings, strings of length 1
+    
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
+ 
+
+
+
+
+def main():
+    sentence = input('Enter your sentence: ')
+    camelcased = camel_case(sentence)
+    print(camelcased)
+
+
+if __name__ == '__main__':
+    main()        
---
+++ test_camel_case.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest import TestCase
+
+import camel_case
+
+
+class TestCamelCase(TestCase):
+
+    def test_camelcase_sentence(self):
+        self.assertEqual('helloWorld', camel_case.camel_case('Hello World'))
+
+    def test_empty_string_value_error(self):
+        with self.assertRaises(ValueError):
+             camel_case.camel_case('') 
+
+    def test_string_with_numbers(self):
+        with self.assertRaises(ValueError):
+             camel_case.camel_case('1532') 
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
